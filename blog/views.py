9cdd5a2276ffa30568ace86dadd011815db7a690
Sahil Kumar <sahil1678kumar@gmail.com>
2021-03-03 23:40:08 +0530

    post_new to postNew

--- blog/views.py
+++ blog/views.py
@@ -30,7 +30,7 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts':posts})
 	
-def post_new(request):
+def postNew(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
 		if form.is_valid():
